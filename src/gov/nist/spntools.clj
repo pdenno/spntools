73e21a80336c81655fb888c6e5bca9a4f27ccb3f
Peter Denno <podenno@gmail.com>
2017-02-10 17:59:38 -0500

    join reduction WIP 2

--- src/gov/nist/spntools.clj
+++ src/gov/nist/spntools.clj
@@ -1,6 +1,7 @@
 (ns gov.nist.spntools
   (:require [clojure.data.xml :as xml :refer (parse-str)]
             [clojure.pprint :refer (cl-format pprint pp)]
+            [clojure.math.combinatorics :as combo]
             [gov.nist.spntools.util.pnml :refer (read-pnml reorder-marking ppp ppprint)]))
 
 (defn initial-marking
@@ -44,7 +45,7 @@
   "Return true if transition is fireable under the argument marking."
   [pn mark tid]
   (when-let [arcs (not-empty (arcs-into-trans pn tid))]
-    (every? (fn [ar] (>= (nth mark (dec (:pid (id2place pn (:source ar)))))
+    (every? (fn [ar] (>= (nth mark (dec (:pid (name2place pn (:source ar)))))
                          (:multiplicity ar)))
             arcs)))
 
@@ -62,12 +63,12 @@
   [pn [mark tid]]
   (as-> mark ?m
     (reduce (fn [mar arc]
-              (let [indx (dec (:pid (id2place pn (:source arc))))] 
+              (let [indx (dec (:pid (name2place pn (:source arc))))] 
                 (update mar indx #(- % (:multiplicity arc)))))
             ?m
             (arcs-into-trans pn tid))
     (reduce (fn [mar arc]
-              (let [indx (dec (:pid (id2place pn (:target arc))))] 
+              (let [indx (dec (:pid (name2place pn (:target arc))))] 
                 (update mar indx #(+ % (:multiplicity arc)))))
             ?m
             (arcs-outof-trans pn tid))))
@@ -93,7 +94,7 @@
 (defn calc-reachability-aux
   [pn marking]
   (let [nexts (next-markings pn marking)] 
-    (swap! *visited-links* into (map #(list (:source %) (:tid (id2transition pn (:trans %)))) nexts))
+    (swap! *visited-links* into (map #(list (:source %) (:tid (name2transition pn (:trans %)))) nexts))
     (swap! *graph* into nexts)
     (doseq [nx nexts]
       (calc-reachability-aux pn (:target nx)))))
@@ -184,7 +185,7 @@
 ;;;==========================================================================
 ;;; GSPN --> SPN
 ;;;==========================================================================
-(declare split2spn find-splits join2spn find-joins eliminate-pn add-pn)
+(declare split2spn find-splits join2spn find-joins eliminate-pn add-pn make-join-trans)
 (defn gspn2spn [pn]
   (->
    (split2spn pn)
@@ -265,6 +266,105 @@
 ;;;      |   place-out-in
 ;;;     _V_    
 ;;;    (___)   place-out      [Keep]
+
+(defn max-tid [pn]
+  (apply max (map :tid (:transitions pn))))
+
+#_(defn join-trans-name
+  [trans-set owner other]
+  (keyword (str (subs (str (:name owner)) 1)
+                "-ord-"
+                (inc (.indexOf trans-set other)))))
+
+;;;  - If you are first, you inhibit everyone else, and you were inhibited by everyone else.
+;;;  - If you are last, everyone else was activated (arcs into you) and you arc-out to :place-out
+;;;  - If you are in the middle, you inhibit everyone behind you, but who you have behind you
+;;;    depends on who is ahead of you. To enumerate this scenario, select from all permuations
+;;;    those for which you are in that middle position.
+
+
+;;; For join reduction, you need separate logic for each permutation of 
+;;; the ordering where there is a choice of who is in front of you and who behind you.
+;;; (Thus no chioce if you are first or last; everyone is behind you and in front of you, respectively.)
+;;; I think the method of implmentation is to create equivalence classes with
+;;; sets of things in front of you and behind you using combinations. 
+;;; 
+;;;      - Receive activators from everyone in front of you. 
+;;;      - Receive inhibitors from everyone behind you.
+;;;      - Send activator to your target (if not last) or to the final target otherwise.
+
+
+;;; Looking at the set minus you, look at combinations for length, length-1, ... 0
+;;; These combinations are the places behind you, back fill with those in front of you. 
+
+;;; Naming convention for transitions: who is ahead of you?
+(defn new-name
+  "Return the string naming the keyword."
+  [key suffix]
+  (keyword (str (str (subs (str key) 1)) suffix)))
+
+
+;;; POD change from places to (:iplaces binds) once it works
+(defn unique-orders
+  "Return a collection of arcs and transitions for owner (which names a iplace)."
+  [binds owner]
+  (let [others (remove #(= % owner) (:places* binds))
+        iplace (some #(when (= (:name %) owner) %) (:iplaces binds))]
+    (loop [in-front 0
+           accum []]
+      (if (> in-front (count others))
+        accum
+        (cond
+          (= in-front 0)
+          (recur (inc in-front)
+                 (conj accum {:name (new-name owner "-first")
+                              :receive-inhibitors others
+                              :send-activator (:name (:iplace binds))}))
+          (= in-front (count others))
+          (recur (inc in-front)
+                 (conj accum {:name (new-name owner "-last")
+                              :recieve-activators others
+                              :send-activator (:name (:place-out binds))}))
+          :default
+          (recur (inc in-front)
+                 (reduce (fn [acc ahead]
+                           (conj acc {:name 
+
+
+
+                                 accum
+                           (combo/combinations others in-front))]
+                                
+                     
+  
+
+
+  
+  
+  
+(defn make-join-trans
+  "Make a set of replacement transitions for each of the arguments. There are
+   as many replacements in each set as there are transitions in the argument.
+   Provide an extra attribute to help make the connections."
+  [pn binds]
+  (let [result (atom [])
+        tid-cnt (atom (max-tid pn))]
+    (reduce (fn [res [owner other]]
+              (conj res {:name (join-trans-name trans-set owner other)
+                         :tid (swap! tid-cnt inc)
+                         :rate (:rate owner)
+                         :owner (:name owner)
+                         :order (.indexOf trans-set other)
+                         :type :exponential}))
+            []
+            (unique-orders (:places* binds)))))
+
+
+
+  
+
+
+
 (defn join-binds
   "Return a map identifying a set of things involved in the pattern shown above."
   [pn imm]
@@ -283,25 +383,9 @@
       (assoc ?b :place-out-in (some #(when (= (:source %) (:imm-name ?b)) %) arcs))
       (assoc ?b :place-out (some #(when (= (:name %) (:target (:place-out-in ?b))) %) places)))))
 
-(defn join2spn
-  "Eliminate IMMs that have outbound arcs to multiple places and (a single???) inbound arc."
-  [pn]
-  (let [arcs (:arcs pn)
-        trs (:transitions pn)
-        places (:places pn)
-        new-cnt (atom 0)]
-    (reduce (fn [pn imm]
-              (let [binds (join-binds pn imm)]
-                (as-> pn ?pn
-                  (eliminate-pn ?pn imm)
-                  (reduce (fn [pn ar] (eliminate-pn pn ar)) ?pn (:top-ins b))
-                  (reduce (fn [pn tr] (eliminate-pn pn tr)) ?pn (:trans b))
-                  (reduce (fn [pn ar] (eliminate-pn pn ar)) ?pn (:places-ins b))
-                  (reduce (fn [pn ar] (eliminate-pn pn ar)) ?pn (:imm-ins b))
-                  (eliminate-pn ?pn (:place-out-in b))
-                
-            pn
-            (find-joins pn))))))
+
+; (def bbb (join-binds pn2 (first (find-joins pn2))))
+; (calc-join-trans pn2 bbb)
 
 (defn find-joins
   "Return IMMs that have multiple inbound arcs."
@@ -312,6 +396,27 @@
                 (and (immediate? pn tr-name)
                      (> (count (filter (fn [ar] (= (:target ar) tr-name)) arcs)) 1))))
             (:transitions pn))))
+
+(defn join2spn
+  "Eliminate IMMs that have outbound arcs to multiple places and (a single???) inbound arc."
+  [pn]
+  (let [arcs (:arcs pn)
+        trs (:transitions pn)
+        places (:places pn)
+        new-cnt (atom 0)]
+    (reduce (fn [pn imm]
+              (let [b (join-binds pn imm)]
+                (as-> pn ?pn
+                  (eliminate-pn ?pn imm)
+                  (reduce (fn [pn ar] (eliminate-pn pn ar)) ?pn (:top-ins b))
+                  (reduce (fn [pn tr] (eliminate-pn pn tr)) ?pn (:trans b))
+                  (reduce (fn [pn ar] (eliminate-pn pn ar)) ?pn (:places-ins b))
+                  (reduce (fn [pn ar] (eliminate-pn pn ar)) ?pn (:imm-ins b))
+                  (eliminate-pn ?pn (:place-out-in b))
+                  (let [new-trans (make-join-trans pn (:trans b))]
+                    ?pn))))
+            pn
+            (find-joins pn))))
 
 ;;; ---- Utils ---------------
 (defn eliminate-pn
