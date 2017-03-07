cef936438ab82e594f53f446dd0469e0f1777e7e
Peter Denno <podenno@gmail.com>
2017-03-07 11:17:53 -0500

    new tests

--- test/gov/nist/spntools_test.clj
+++ test/gov/nist/spntools_test.clj
@@ -19,7 +19,7 @@
       (is (= (count (:transitions j3)) 13))
       (is (= (count (:places j3)) 5)))))
 
-#_(deftest join2-graph-test
+(deftest join2-graph-test
   (testing "join graph equality"
     (let [result (gspn2spn (read-pnml "data/join2.xml"))]
       (is (= (set (:places result))
@@ -28,30 +28,126 @@
                {:name :Pjoin, :pid 3, :initial-marking 0}
                {:name :Pstart, :pid 4, :initial-marking 2}}))
       (is (= (set (:transitions result))
-             #{{:name :Treturn, :tid 4, :type :exponential, :rate 1.0}
-               {:name :P1-first, :tid 5, :type :exponential, :rate 1.0}
-               {:name :P1-last, :tid 6, :type :exponential, :rate 1.0}
-               {:name :P2-first, :tid 7, :type :exponential, :rate 1.0}
-               {:name :P2-last, :tid 8, :type :exponential, :rate 1.0}}))
+             #{{:name :Treturn, :tid 8, :type :exponential, :rate 1.0}
+               {:name :IMM-P1-first, :tid 9, :type :exponential, :rate 1.0}
+               {:name :IMM-P1-last, :tid 10, :type :exponential, :rate 1.0}
+               {:name :IMM-P2-first, :tid 11, :type :exponential, :rate 1.0}
+               {:name :IMM-P2-last, :tid 12, :type :exponential, :rate 1.0}}))
       (is (= (set (:arcs result))
-             #{{:aid 6, :source :Pjoin, :target :Treturn, :type :normal, :multiplicity 1}
-               {:aid 11, :source :Treturn, :target :Pstart, :type :normal, :multiplicity 2}
-               {:aid 12, :source :Pstart, :target :P1-first, :type :normal, :multiplicity 1}
-               {:aid 13, :source :P1, :target :P1-first, :type :inhibitor, :multiplicity 1}
-               {:aid 14, :source :P1-first, :target :P1, :type :normal, :multiplicity 1}
-               {:aid 15, :source :P2, :target :P1-first, :type :inhibitor, :multiplicity 1}
-               {:aid 16, :source :Pstart, :target :P1-last, :type :normal, :multiplicity 1}
-               {:aid 17, :source :P1, :target :P1-last, :type :inhibitor, :multiplicity 1}
-               {:aid 18, :source :P1-last, :target :Pjoin, :type :normal, :multiplicity 1}
-               {:aid 19, :source :P2, :target :P1-last, :type :normal, :multiplicity 1}
-               {:aid 20, :source :Pstart, :target :P2-first, :type :normal, :multiplicity 1}
-               {:aid 21, :source :P2, :target :P2-first, :type :inhibitor, :multiplicity 1}
-               {:aid 22, :source :P2-first, :target :P2, :type :normal, :multiplicity 1}
-               {:aid 23, :source :P1, :target :P2-first, :type :inhibitor, :multiplicity 1}
-               {:aid 24, :source :Pstart, :target :P2-last, :type :normal, :multiplicity 1}
-               {:aid 25, :source :P2, :target :P2-last, :type :inhibitor, :multiplicity 1}
-               {:aid 26, :source :P2-last, :target :Pjoin, :type :normal, :multiplicity 1}
-               {:aid 27, :source :P1, :target :P2-last, :type :normal, :multiplicity 1}})))))
+             #{{:aid 14, :source :Pjoin, :target :Treturn, :name :aa-14, :type :normal, :multiplicity 1}
+               {:aid 19, :source :Treturn, :target :Pstart, :name :aa-19, :type :normal, :multiplicity 2}
+               {:aid 20, :source :Pstart, :target :IMM-P1-first, :name :aa-20, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 21, :source :IMM-P1-first, :target :P1, :name :aa-21, :type :normal, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 22, :source :P1, :target :IMM-P1-first, :name :aa-22, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 23, :source :P2, :target :IMM-P1-first, :name :aa-23, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 24, :source :Pstart, :target :IMM-P1-last, :name :aa-24, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 25, :source :IMM-P1-last, :target :Pjoin, :name :aa-25, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 26, :source :P1, :target :IMM-P1-last, :name :aa-26, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 27, :source :P2, :target :IMM-P1-last, :name :aa-27, :type :normal, :multiplicity 1, :debug :receive-activators}
+               {:aid 28, :source :Pstart, :target :IMM-P2-first, :name :aa-28, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 29, :source :IMM-P2-first, :target :P2, :name :aa-29, :type :normal, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 30, :source :P2, :target :IMM-P2-first, :name :aa-30, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 31, :source :P1, :target :IMM-P2-first, :name :aa-31, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 32, :source :Pstart, :target :IMM-P2-last, :name :aa-32, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 33, :source :IMM-P2-last, :target :Pjoin, :name :aa-33, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 34, :source :P2, :target :IMM-P2-last, :name :aa-34, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 35, :source :P1, :target :IMM-P2-last, :name :aa-35, :type :normal, :multiplicity 1, :debug :receive-activators}})))))
+
+
+
+(deftest join3-graph-test
+  (testing "join graph equality (join3)"
+    (let [result (gspn2spn (read-pnml "data/join3.xml"))]
+      (is (= (set (:places result))
+             #{{:name :P1, :pid 1, :initial-marking 0}
+               {:name :P2, :pid 2, :initial-marking 0}
+               {:name :P3, :pid 3, :initial-marking 0}
+               {:name :Pjoin, :pid 4, :initial-marking 0}
+               {:name :Pstart, :pid 5, :initial-marking 3}}))
+      (is (= (set (:transitions result))
+             #{{:name :Tassemble, :tid 10, :type :exponential, :rate 1.0}
+               {:name :IMM-P1-first, :tid 11, :type :exponential, :rate 1.0}
+               {:name :IMM-P1--P3-before, :tid 12, :type :exponential, :rate 1.0}
+               {:name :IMM-P1--P2-before, :tid 13, :type :exponential, :rate 1.0}
+               {:name :IMM-P1-last, :tid 14, :type :exponential, :rate 1.0}
+               {:name :IMM-P3-first, :tid 15, :type :exponential, :rate 1.0}
+               {:name :IMM-P3--P1-before, :tid 16, :type :exponential, :rate 1.0}
+               {:name :IMM-P3--P2-before, :tid 17, :type :exponential, :rate 1.0}
+               {:name :IMM-P3-last, :tid 18, :type :exponential, :rate 1.0}
+               {:name :IMM-P2-first, :tid 19, :type :exponential, :rate 1.0}
+               {:name :IMM-P2--P1-before, :tid 20, :type :exponential, :rate 1.0}
+               {:name :IMM-P2--P3-before, :tid 21, :type :exponential, :rate 1.0}
+               {:name :IMM-P2-last, :tid 22, :type :exponential, :rate 1.0}}))
+      (is (= (set (:arcs result))
+             #{{:aid 18, :source :Pjoin, :target :Tassemble, :name :aa-18, :type :normal, :multiplicity 1}
+               {:aid 25, :source :Tassemble, :target :Pstart, :name :aa-25, :type :normal, :multiplicity 3}
+               {:aid 26, :source :Pstart, :target :IMM-P1-first, :name :aa-26, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 27, :source :IMM-P1-first, :target :P1, :name :aa-27, :type :normal, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 28, :source :P1, :target :IMM-P1-first, :name :aa-28, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 29, :source :P3, :target :IMM-P1-first, :name :aa-29, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 30, :source :P2, :target :IMM-P1-first, :name :aa-30, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 31, :source :Pstart, :target :IMM-P1--P3-before, :name :aa-31, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 32, :source :P1, :target :IMM-P1--P3-before, :name :aa-32, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 33, :source :IMM-P1--P3-before, :target :P1, :name :aa-33, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 34, :source :P2, :target :IMM-P1--P3-before, :name :aa-34, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 35, :source :IMM-P1--P3-before, :target :P3, :name :aa-35, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 36, :source :P3, :target :IMM-P1--P3-before, :name :aa-36, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 37, :source :Pstart, :target :IMM-P1--P2-before, :name :aa-37, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 38, :source :P1, :target :IMM-P1--P2-before, :name :aa-38, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 39, :source :IMM-P1--P2-before, :target :P1, :name :aa-39, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 40, :source :P3, :target :IMM-P1--P2-before, :name :aa-40, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 41, :source :IMM-P1--P2-before, :target :P2, :name :aa-41, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 42, :source :P2, :target :IMM-P1--P2-before, :name :aa-42, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 43, :source :Pstart, :target :IMM-P1-last, :name :aa-43, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 44, :source :IMM-P1-last, :target :Pjoin, :name :aa-44, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 45, :source :P1, :target :IMM-P1-last, :name :aa-45, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 46, :source :P3, :target :IMM-P1-last, :name :aa-46, :type :normal, :multiplicity 1, :debug :receive-activators}
+               {:aid 47, :source :P2, :target :IMM-P1-last, :name :aa-47, :type :normal, :multiplicity 1, :debug :receive-activators}
+               {:aid 48, :source :Pstart, :target :IMM-P3-first, :name :aa-48, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 49, :source :IMM-P3-first, :target :P3, :name :aa-49, :type :normal, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 50, :source :P3, :target :IMM-P3-first, :name :aa-50, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 51, :source :P1, :target :IMM-P3-first, :name :aa-51, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 52, :source :P2, :target :IMM-P3-first, :name :aa-52, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 53, :source :Pstart, :target :IMM-P3--P1-before, :name :aa-53, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 54, :source :P3, :target :IMM-P3--P1-before, :name :aa-54, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 55, :source :IMM-P3--P1-before, :target :P3, :name :aa-55, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 56, :source :P2, :target :IMM-P3--P1-before, :name :aa-56, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 57, :source :IMM-P3--P1-before, :target :P1, :name :aa-57, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 58, :source :P1, :target :IMM-P3--P1-before, :name :aa-58, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 59, :source :Pstart, :target :IMM-P3--P2-before, :name :aa-59, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 60, :source :P3, :target :IMM-P3--P2-before, :name :aa-60, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 61, :source :IMM-P3--P2-before, :target :P3, :name :aa-61, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 62, :source :P1, :target :IMM-P3--P2-before, :name :aa-62, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 63, :source :IMM-P3--P2-before, :target :P2, :name :aa-63, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 64, :source :P2, :target :IMM-P3--P2-before, :name :aa-64, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 65, :source :Pstart, :target :IMM-P3-last, :name :aa-65, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 66, :source :IMM-P3-last, :target :Pjoin, :name :aa-66, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 67, :source :P3, :target :IMM-P3-last, :name :aa-67, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 68, :source :P1, :target :IMM-P3-last, :name :aa-68, :type :normal, :multiplicity 1, :debug :receive-activators}
+               {:aid 69, :source :P2, :target :IMM-P3-last, :name :aa-69, :type :normal, :multiplicity 1, :debug :receive-activators}
+               {:aid 70, :source :Pstart, :target :IMM-P2-first, :name :aa-70, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 71, :source :IMM-P2-first, :target :P2, :name :aa-71, :type :normal, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 72, :source :P2, :target :IMM-P2-first, :name :aa-72, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 73, :source :P1, :target :IMM-P2-first, :name :aa-73, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 74, :source :P3, :target :IMM-P2-first, :name :aa-74, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 75, :source :Pstart, :target :IMM-P2--P1-before, :name :aa-75, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 76, :source :P2, :target :IMM-P2--P1-before, :name :aa-76, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 77, :source :IMM-P2--P1-before, :target :P2, :name :aa-77, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 78, :source :P3, :target :IMM-P2--P1-before, :name :aa-78, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 79, :source :IMM-P2--P1-before, :target :P1, :name :aa-79, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 80, :source :P1, :target :IMM-P2--P1-before, :name :aa-80, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 81, :source :Pstart, :target :IMM-P2--P3-before, :name :aa-81, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 82, :source :P2, :target :IMM-P2--P3-before, :name :aa-82, :type :inhibitor, :multiplicity 1, :debug :psv-trans&places}
+               {:aid 83, :source :IMM-P2--P3-before, :target :P2, :name :aa-83, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 84, :source :P1, :target :IMM-P2--P3-before, :name :aa-84, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 85, :source :IMM-P2--P3-before, :target :P3, :name :aa-85, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 86, :source :P3, :target :IMM-P2--P3-before, :name :aa-86, :type :normal, :multiplicity 1, :debug :s&r}
+               {:aid 87, :source :Pstart, :target :IMM-P2-last, :name :aa-87, :type :normal, :multiplicity 1, :debug :receive-tops}
+               {:aid 88, :source :IMM-P2-last, :target :Pjoin, :name :aa-88, :type :normal, :multiplicity 1, :debug :send-activators}
+               {:aid 89, :source :P2, :target :IMM-P2-last, :name :aa-89, :type :inhibitor, :multiplicity 1, :debug :receive-inhibitors}
+               {:aid 90, :source :P1, :target :IMM-P2-last, :name :aa-90, :type :normal, :multiplicity 1, :debug :receive-activators}
+               {:aid 91, :source :P3, :target :IMM-P2-last, :name :aa-91, :type :normal, :multiplicity 1, :debug :receive-activators}})))))
+
 
 (defn find-arc-test
   [pn source target]
@@ -92,16 +188,6 @@
 ;;;==========================================================================
 ;;; Reduction GSPN to SPN, one-step, two-step and full
 ;;;==========================================================================
-
-;;; Ones with commas are inhibitors
-#_(defn j2-has-arcs-test []    
-  (let [j2 (gspn2spn (read-pnml "data/join2.xml"))
-        data  [[:Pstart :P1-last 1] [:Pstart :P1-first 2] [:Pstart :P2-first 3] [:Pstart :P2-last 4]
-               [:P1-first :P1 5]  [:P1 :P1-first, 6] [:P2 :P1-first 7]  [:P1 :P2-first, 8]  [:P2-first :P2 9]
-               [:P2 :P2-first, 10] [:P2 :P1-last 11] [:P1 :P2-last 12] [:P1 :P1-last, 13] [:P2 :P2-last, 14]
-               [:P1-last :Pjoin 15]  [:P2-last :Pjoin 16] [:Pjoin :Treturn 17]  [:Treturn :Pstart 18]]]
-    (println "Testing" (count data) "arcs")
-    (check-every j2 data)))
 
 #_(defn marsan-one-step-arcs-test []    
   (let [j2 (one-step "data/marsan69.xml")
@@ -162,7 +248,6 @@
 
 #_(deftest find-missing-arcs
   (testing "PNs have all correct arcs"
-    (is (j2-has-arcs-test))
     (is (marsan-one-step-arcs-test))))
 
 #_(deftest find-unwanted-arcs
@@ -261,12 +346,10 @@
 (deftest steady-state-properties-join3
   (testing "Steady-state properties are consistent/join3"
     (let [result (:avg-tokens-on-place (pn-steady-state (read-pnml "data/join3.xml")))
-          correct {:P1 0.24779 :P2 0.27764 :P3 0.3029 :Pjoin 0.39401 :Pstart 0.98963}]
+          correct {:P1 0.29412 :P2 0.29412 :P3 0.29412 :Pjoin 0.35294 :Pstart 1.05882}]
       (is (every? (fn [[key val]]
                     (=* val (get correct key) 0.0001)) result)))))
 
-;;; There must be a bug my drawing of join3-reduce because the values 
-;;; here should match those of join3. 
 (deftest steady-state-properties-join3-reduced
   (testing "Steady-state properties are consistent/join3-reduce"
     (let [result (:avg-tokens-on-place (pn-steady-state (read-pnml "data/join3-reduce-v2.xml")))
