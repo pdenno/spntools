7eedcfeb7d1d7d8c7241304ec5dfa0549a43f5a7
Peter Denno <podenno@gmail.com>
2017-06-15 16:36:59 -0400

    New m3-feeder.xml which BTW demonstrates a bug in PIPE

--- src/gov/nist/spntools/util/simulate.clj
+++ src/gov/nist/spntools/util/simulate.clj
@@ -1,38 +1,10 @@
 (ns gov.nist.spntools.util.simulate
   (:require [clojure.pprint :refer (cl-format pprint pp)]
             [gov.nist.spntools.util.reach :as pnr]
-            [gov.nist.spntools.util.utils :as pnu]
+            [gov.nist.spntools.util.utils :as pnu :refer (as-pn-ok-> name2obj)]
             [gov.nist.spntools.util.pnml  :as pnml]))
 
 ;;; Purpose: Run a PN, producing a log of its execution.
-
-(def talking-m2-bas
-  (pnr/renumber-pids 
-   {:places                                         
-    [{:name :buffer, :pid 1, :initial-tokens 1    }
-     {:name :m1-blocked, :pid 2, :initial-tokens 0}
-     {:name :m1-busy, :pid 3, :initial-tokens 1   }
-     {:name :m2-busy, :pid 4, :initial-tokens 1   }
-     {:name :m2-starved, :pid 5, :initial-tokens 0}],
-    :transitions  ; :fn here would be added by GP, associating SCADA acts with transitions.
-    [{:name :m1-complete-job, :tid 6, :type :exponential, :rate 3.9 :fn (fn [tkns] {:act :bj :tkns tkns})}
-     {:name :m1-start-job, :tid 7, :type :immediate, :rate 1.0      :fn (fn [tkns] {:act :aj :tkns tkns})}
-     {:name :m2-complete-job, :tid 8, :type :exponential, :rate 1.0 :fn (fn [tkns] {:act :ej :tkns tkns})}
-     {:name :m2-start-job, :tid 9, :type :immediate, :rate 1.0      :fn (fn [tkns] {:act :sm :tkns tkns})}],
-    :arcs ; :bind here would be added by GP, selecting where to make intro and elim, split jobtypes, etc. 
-    [{:aid 10, :source :buffer, :target :m1-start-job, :name :aa-10, :type :inhibitor, :multiplicity 5 :bind {:type :a}}
-     {:aid 11, :source :buffer, :target :m2-start-job, :name :aa-11, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 12, :source :m1-blocked, :target :m1-start-job, :name :aa-12, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 13, :source :m1-busy, :target :m1-complete-job, :name :aa-13, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 14, :source :m1-complete-job, :target :m1-blocked, :name :aa-14, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 15, :source :m1-start-job, :target :buffer, :name :aa-15, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 16, :source :m1-start-job, :target :m1-busy, :name :aa-16, :type :normal, :multiplicity 1
-      :bind {:type :a :act :intro}}
-     {:aid 17, :source :m2-busy, :target :m2-complete-job, :name :aa-17, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 18, :source :m2-complete-job, :target :m2-starved, :name :aa-18, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 19, :source :m2-start-job, :target :m2-busy, :name :aa-19, :type :normal, :multiplicity 1 :bind {:type :a}}
-     {:aid 20, :source :m2-starved, :target :m2-start-job, :name :aa-20, :type :normal, :multiplicity 1
-      :bind {:type :a :act :elim}}]}))
 
 ;;; In ordinary GSPN code, a marking (:state) is just a vector of integers signifying the tokens on a place.
 ;;; In our implementation, we instead keep maps of queues containing tokens. Where we need the ordinary GSPN marking,
